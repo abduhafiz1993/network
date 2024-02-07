23089be189fb6f68c4d98edb0b5a4baaf035e25f
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2024-02-07 12:53:24 -0800

    makeing migration

--- network/models.py
+++ network/models.py
@@ -1,14 +1,16 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
+## user class
 class User(AbstractUser):
     pass
 
 
+##  post model
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
+    # like have many to many relation to user 
     likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
@@ -16,10 +18,10 @@ class Post(models.Model):
         return f"Content: {self.content} likes: {self.likes}"
 
 class Comment(models.Model):
-    text = models.TextField()
+    comment_text = models.TextField()
     comment_time = models.DateTimeField(auto_now_add=True)
     commenter = models.ForeignKey(User, on_delete=models.CASCADE)
-    Post_List = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     
     def __str__(self):
         return f"Text: {self.text} comment_time: {self.comment_time} commenter:{self.commenter} auction_listing: {self.auction_listing}"
