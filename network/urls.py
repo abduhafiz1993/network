ec840ec0ee70e65eb9da74c6f8413a4ef6d2c46d
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2024-02-10 10:48:05 -0800

    update on views file

--- network/urls.py
+++ network/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+    path("allpost", views.allpost, name="allpost"),
+    
 ]
